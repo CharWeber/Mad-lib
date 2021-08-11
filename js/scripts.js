cce48e5c5bc94dcb4e975da4e2ee5330597cbfdf
Charlie Weber <charlestweber@gmail.com>
2021-08-11 10:49:54 -0700

    changed html for debug exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    event.preventDefault();
+  $("#formOne").submit(function() {
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
@@ -8,46 +7,13 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
+    $("person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
 
-    $("#story").show();
-  });
-
-  $("#formTwo").submit(function(event){
-    event.preventDefault();
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput = $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb1").val();
-    const nounInput = $("input#noun").val();
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb1").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story2").show();
-
-  });
-  $("#form3").submit(function(event){
-    event.preventDefault();
-    const person1Input = $("input#person1").val();
-    $(".person1").text(person1Input)
-    $("#story2").show();
-  });
-  $("#form4").submit(function(event){
-    event.preventDefault();
-    const shoutInput = $("input#loud").val();
-    
-    $(".loud").text(shoutInput.toUpperCase());
-    $("#shout").show();
+    $("#story").sho();
   });
 });

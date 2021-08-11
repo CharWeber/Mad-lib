6a2541bb4170ea21000f941dcdee7cd8b715f110
Charlie Weber <charlestweber@gmail.com>
2021-08-11 09:40:09 -0700

    add second mad-lib html with js

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,23 @@ $(document).ready(function() {
 
     $("#story").show();
   });
+
+  $("#formTwo").submit(function(event){
+    event.preventDefault();
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput = $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb1").val();
+    const nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb1").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story2").show();
+  })
 });
